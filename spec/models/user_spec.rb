2cf3f3e8d077da3dec0bfc68aca2ca096ab5d0e4
Moacir Petry <m.petry9@gmail.com>
2025-02-05 12:55:32 -0300

    fix spec error

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it 'is valid with valid attributes' do
     user = User.new(name: 'John Doe', email: 'john@example.com')
-    expect(user).not_to be_valid
-    # There's nothing over here, it's just to test a branch rule
+    expect(user).to be_valid
   end
 
   it 'is not valid without a name' do
