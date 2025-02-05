c2586967a08980b9a945be5f386797f4713865bb
Moacir Petry <m.petry9@gmail.com>
2025-02-05 10:58:19 -0300

    force failure

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it 'is valid with valid attributes' do
     user = User.new(name: 'John Doe', email: 'john@example.com')
-    expect(user).to be_valid
+    expect(user).not_to be_valid
   end
 
   it 'is not valid without a name' do
